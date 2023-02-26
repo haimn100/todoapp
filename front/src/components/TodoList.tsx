0aab326bb4aa411d047f245386745fe827b0b9ed
haim <haimn100@gmail.com>
2023-02-26 17:36:26 +0200

    added store

--- front/src/components/TodoList.tsx
+++ front/src/components/TodoList.tsx
@@ -1,9 +1,13 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
+import { todoApi } from '../store'
 import styles from './TodoList.module.css';
 import TodoItem from './TodoItem';
 
 
 const TodoList = () => {
+
+    const { data: todos, isLoading, isError } = todoApi.useGetAllQuery();
+    const [addTodo] = todoApi.useAddTodoMutation();
 
     const [task, setTask] = useState("");
 
@@ -13,16 +17,33 @@ const TodoList = () => {
 
     const add = async (e: FormEvent<HTMLButtonElement>) => {
         e.preventDefault();
+        if (isValidTask(task)) await addTodo(task);
         setTask("");
     };
 
+    const isValidTask = (task: string): boolean => {
+        return task.length > 0 && todos?.find(el => el.task === task) === undefined;
+    }
 
+    if (isError) {
+        return <span>An error has occured</span>
+    }
     return (
         <div className={styles.container}>
             <form className="App-form">
                 <input type="text" className={styles.inputTask} value={task} onChange={onInputTask} />
                 <button type="submit" onClick={add}>+</button>
             </form>
+            {
+                isLoading ?
+                    <span>Loading...</span>
+                    :
+                    todos?.length === 0 ?
+                        <p>Start adding Todo's</p>
+                        :
+                        <ul> {todos?.map(todo => <TodoItem key={todo.id} todo={todo} isValidTask={isValidTask} />)}</ul>
+            }
+
         </div>
     )
 
--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,6 +1,50 @@
+import { createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
 export type Todo = {
     id:number,
     task:string,
     completed:boolean;
     created_at:Date;
   }
+
+export const todoApi = createApi({
+    reducerPath: 'todo',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1212/api/' }),
+    tagTypes:['Todos'],
+    endpoints: (builder) => ({
+      getAll: builder.query<Todo[], void>({
+        query: () => 'todos',
+        providesTags:[{type:'Todos'}],
+        transformResponse: (response:Todo[]) => response.sort((a,b)=> new Date(b.created_at).getTime() - new Date(a .created_at).getTime())        
+      }),
+      addTodo: builder.mutation<string,string>({
+        query(task) {
+            return {
+                url: 'todos',
+                method: 'POST',
+                body: {task}
+            };
+        },
+        invalidatesTags: ['Todos']
+      }),
+      updateTodo: builder.mutation<Todo,Todo>({
+        query(todo) {
+            return {
+                url: `todos/${todo.id}`,
+                method: 'PUT',
+                body: todo
+            };
+        },
+        invalidatesTags: ['Todos']
+      }),
+      deleteTodo: builder.mutation<number,number>({
+        query(id) {
+            return {
+                url: `todos/${id}`,
+                method: 'DELETE'
+            };
+        },
+        invalidatesTags: ['Todos']
+      })
+    }),
+  });
